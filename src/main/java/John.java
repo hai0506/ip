74f337334e16c7fe93b20e193785e5d948c9fc8e
Hai <hoanghai050605@gmail.com>
2025-08-18 16:25:55 +0800

    set bot to echo messages

--- src/main/java/John.java
+++ src/main/java/John.java
@@ -1,6 +1,26 @@
+import java.util.Locale;
+import java.util.Scanner;
 public class John {
+    public static void printHLine() {
+        System.out.println("---------------------------------------");
+    }
     public static void main(String[] args) {
-        System.out.println("Hi I'm John. How can I help you?");
-        System.out.println("Bye!");
+        printHLine();
+        System.out.println("    Hi I'm John.");
+        System.out.println("    How can I help you?");
+        printHLine();
+        System.out.println();
+        Scanner sc = new Scanner(System.in);
+        String prompt = sc.nextLine();
+        while(!prompt.toLowerCase().equals("bye")) {
+            printHLine();
+            System.out.println("    " + prompt);
+            printHLine();
+            System.out.println();
+            prompt = sc.nextLine();
+        }
+        printHLine();
+        System.out.println("     Bye!");
+        printHLine();
     }
 }
