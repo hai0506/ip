8b1bdf19220749e184e60c1402375d76713aeaf7
Hai <hoanghai050605@gmail.com>
2025-08-18 21:59:10 +0800

    create tasks and implement mark as done

--- src/main/java/John.java
+++ src/main/java/John.java
@@ -1,11 +1,11 @@
-import java.util.Locale;
 import java.util.Scanner;
+
 public class John {
     public static void printHLine() {
         System.out.println("---------------------------------------");
     }
     public static void main(String[] args) {
-        String[] list = new String[100];
+        Task[] list = new Task[100];
         int listIndex = 0;
         printHLine();
         System.out.println("    Hi I'm John.");
@@ -16,14 +16,36 @@ public class John {
         String prompt = sc.nextLine();
         while(!prompt.toLowerCase().equals("bye")) {
             printHLine();
-            if(prompt.toLowerCase().equals("list")) {
+            if(prompt.toLowerCase().matches("^mark\\s\\d+$")) {
+                int index = Integer.parseInt(prompt.split(" ")[1]);
+                if(index > listIndex || index <= 0) {
+                    System.out.println("Task does not exist.");
+                }
+                else {
+                    System.out.println("    I've marked this task as done:");
+                    list[index - 1].mark();
+                    System.out.println("    " + list[index - 1]);
+                }
+            }
+            else if(prompt.toLowerCase().matches("^unmark\\s\\d+$")) {
+                int index = Integer.parseInt(prompt.split(" ")[1]);
+                if(index > listIndex || index <= 0) {
+                    System.out.println("Task does not exist.");
+                }
+                else {
+                    System.out.println("    I've marked this task as not done yet:");
+                    list[index - 1].unMark();
+                    System.out.println("    " + list[index - 1]);
+                }
+            }
+            else if(prompt.toLowerCase().equals("list")) {
                 for(int i = 0; i < listIndex; i++) {
                     System.out.println("    " + (i + 1) + ". " + list[i]);
                 }
             }
             else {
                 System.out.println("    added: " + prompt);
-                list[listIndex] = prompt;
+                list[listIndex] = new Task(prompt);
                 listIndex++;
             }
             printHLine();
---
+++ src/main/java/Task.java
@@ -0,0 +1,20 @@
+public class Task {
+    private String name;
+    private boolean done;
+    public Task(String name) {
+        this.name = name;
+        this.done = false;
+    }
+    public void mark() {
+        this.done = true;
+    }
+    public void unMark() {
+        this.done = false;
+    }
+
+    @Override
+    public String toString() {
+        if(this.done) return "[X] " + name;
+        return "[] " + name;
+    }
+}
